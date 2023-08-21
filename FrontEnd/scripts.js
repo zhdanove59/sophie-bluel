e220482018e5a8449daa1167ea4fd276114f6aa5
Houari <zhdanove@hotmail.fr>
2023-08-21 15:32:05 +0200

    page index avec les boutons

--- FrontEnd/scripts.js
+++ FrontEnd/scripts.js
@@ -20,3 +20,26 @@ async function init(){
 }
 
 init()
+//TESTE
+function displayData(works) {
+  console.log(works)
+  const gallerySurHtml = document.querySelector('.gallery');
+  gallerySurHtml.innerHTML=''
+  let render=""
+  works.forEach(work => {
+    console.log(work)
+    render +=`
+      <figure>
+      <img src="${work.imageUrl}" alt="${work.title}">
+      <figcaption>${work.title}</figcaption>
+    </figure>
+      `
+  });
+gallerySurHtml.innerHTML = render
+}
+async function init(){
+const works=await  getWorks();
+displayData(works)
+}
+
+init()
